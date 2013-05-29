e02c95a5acbaa8d21f58f90dc6e6e42c33f48b03
Suneel Chakravorty <suneel0101@gmail.com>
2013-05-29 00:30:04 -0400

    killing some newlines

--- instrumental.js
+++ instrumental.js
@@ -1,11 +1,8 @@
 (function(global, $){
     function Instrumental(){
         return {
-
             defaultEndpoint: options.defaultEndpoint,
-
             baseData: options.baseData || {},
-
             activate: function () {
                 // for each element that has the data-instrumental-action
                 // set, activate an event listener
@@ -14,15 +11,12 @@
                     this.activateEventListener(elements[i]);
                 }
             },
-
             activateEventListener: function(selector) {
                 // get the element trigger
                 var el = $(selector);
                 var trigger = el.data("instrumental-trigger");
-
                 // get the URL endpoint to which to POST the event data
                 var url = el.data("defaultEndpoint") || this.defaultEndpoint;
-
                 // when the event is triggered, post the data
                 var data = this.getElementData(el);
                 el.on(trigger, function(){
@@ -31,13 +25,10 @@
                         data);
                 });
             },
-
             getElementData: function(el) {
                 var data = el.data();
-
                 // The data dictionary starts off as baseData
                 var instrumentalData = $.extend({}, this.baseData);
-
                 // Construct the data dictionary from the
                 // $.data() values that starts with data-instrumental
                 for (var key in data) {
@@ -46,10 +37,8 @@
                         instrumentalData[instrumentalKey] = data[key];
                     }
                 }
-
                 return instrumentalData;
             }
-
         };
     }
 
